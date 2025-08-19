4b761580dd43296207e53ed89057dadddc10821a
Richard Heuser <richard.heuser@monks.com>
2025-08-19 15:27:11 +0200

    service account gdrive auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,26 @@
 const { GoogleAuth } = require('google-auth-library');
+const path = require('path');
+const fs = require('fs');
 
 let client;
 
 async function authenticate() {
 	if (client) return client;
 	
-	const authClient = new GoogleAuth({
-		scopes: ['https://www.googleapis.com/auth/drive'],
-	});
+	const serviceAccountKeyPath = path.join(__dirname, '../../auth/mantra.json');
+	
+	let authOptions = {
+		scopes: [
+			'https://www.googleapis.com/auth/drive',
+			'https://www.googleapis.com/auth/spreadsheets',
+			'https://www.googleapis.com/auth/documents',
+			'https://www.googleapis.com/auth/cloud-platform'
+		],
+	};
+	
+	if (fs.existsSync(serviceAccountKeyPath)) authOptions.keyFile = serviceAccountKeyPath;
+	
+	const authClient = new GoogleAuth(authOptions);
 	
 	return client = await authClient.getClient();
 }
